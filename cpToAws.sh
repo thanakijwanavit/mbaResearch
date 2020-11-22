b91eee8ae7312a21e6e9e7dfcf9476fefb8877dc
nic <nwanavit@hatari.cc>
2020-11-22 13:46:55 +0700

    completed shoren link for cpToAws

--- cpToAws.sh
+++ cpToAws.sh
@@ -1,5 +1,7 @@
 aws s3 cp ./thesisUpdate.docx s3://nic-clipboard/thesisUpdate.docx&&\
   link=$(aws s3 presign s3://nic-clipboard/thesisUpdate.docx  --expires-in 604800)&&\
+    echo '{"link":"'$link'"}'&&\
   curl --location --request POST 'https://zrm4csjv5a.execute-api.ap-southeast-1.amazonaws.com/Prod/shorten' \
     --header 'Content-Type: application/json' \
-    --data-raw '{"link":"$link"}'|pbcopy
+    --data-raw '{"link":"'$link'"}'| jq -r '.shortLink'|pbcopy &&\
+    pbpaste
