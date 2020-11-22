ba2dc215106ee59a0336518e064c5743616f2e37
nic <nwanavit@hatari.cc>
2020-11-22 13:35:48 +0700

    configure cpToAws

--- cpToAws.sh
+++ cpToAws.sh
@@ -1,3 +1,5 @@
-aws s3 cp ./researchThesisIncomplete.docx s3://nic-clipboard/thesisIncomplete.docx&&\
-  link=$(aws s3 presign s3://nic-clipboard/thesisIncomplete.docx  --expires-in 604800)&&\
-  echo $link|pbcopy
+aws s3 cp ./thesisUpdate.docx s3://nic-clipboard/thesisUpdate.docx&&\
+  link=$(aws s3 presign s3://nic-clipboard/thesisUpdate.docx  --expires-in 604800)&&\
+  curl --location --request POST 'https://zrm4csjv5a.execute-api.ap-southeast-1.amazonaws.com/Prod/shorten' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{"link":"$link"}'|pbcopy
