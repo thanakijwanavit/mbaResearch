6ac9dd90a7b91af8d3ae344f23898b239104f123
nic Wanavit <nic@nics-Mac-mini.local>
2020-12-04 10:36:49 +0700

    change to thesiswithout comment

--- cpToAws.sh
+++ cpToAws.sh
@@ -1,4 +1,4 @@
-aws s3 cp ./thesisUpdate.docx s3://nic-clipboard/thesisUpdate.docx&&\
+aws s3 cp ./thesisWithoutComment.docx s3://nic-clipboard/thesisUpdate.docx&&\
   link=$(aws s3 presign s3://nic-clipboard/thesisUpdate.docx  --expires-in 604800)&&\
     echo '{"link":"'$link'"}'&&\
   curl --location --request POST 'https://zrm4csjv5a.execute-api.ap-southeast-1.amazonaws.com/Prod/shorten' \
